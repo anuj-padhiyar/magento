660dba0b4af6dc41b7fd29bcd63170b404626649
Anuj <padhiyaranuj742000@gmail.com>
2021-05-30 11:54:48 +0530

    final~

--- app/code/local/Ccc/Order/Block/Adminhtml/Order/Cart/Total.php
+++ app/code/local/Ccc/Order/Block/Adminhtml/Order/Cart/Total.php
@@ -11,11 +11,6 @@ class Ccc_Order_Block_Adminhtml_Order_Cart_Total extends Ccc_Order_Block_Adminht
 
     public function getShippingAmount(){
         return $this->getCart()->getShippingAmount();
-        // die;
-        // if($amount = $this->getCart()->getShippingAmount()){
-        //     return $amount;
-        // }
-        // return 0;
     }
 
     public function getFinalTotal(){
